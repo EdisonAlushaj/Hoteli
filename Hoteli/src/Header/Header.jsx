f2ab4f66aa54a810bf47bbd0c74b2ae233ab496f
oltahasimja <oh67751@ubt-uni.net>
2024-03-25 21:36:34 +0100

    Header
    Dizajnimi i header,  drop-down menu: Restauranti, Spa, Sauna, Gym, Pool dhe Activities, butooni i booking, hapsira per logo

---
+++ Hoteli/src/Header/Header.jsx
@@ -0,0 +1,37 @@
+import './Header.css'
+
+function Header() {
+  return (
+    <header className="App-header">
+
+        <div className="logo">
+
+        </div>
+
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            
+            <li> 
+                <div className="dropdown">
+                    <p className="dropbtn">Services	▼ </p>
+                            
+                    <div className="dropdown-content">
+                        <a href="#">Restaurant</a>
+                        <a href="#">Spa</a>
+                        <a href="#">Sauna</a>
+                        <a href="#">Gym</a>
+                        <a href="#">Pool</a>
+                        <a href="#">Activities</a>
+                    </div>
+                </div>
+            </li> 
+            <li><a href="Contact Us">Contact Us</a></li>
+        </ul>
+
+        <button id="booking-btn">BOOK NOW</button>
+    </header>
+  );
+}
+
+export default Header;
