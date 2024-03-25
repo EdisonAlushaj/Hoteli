5eeac29bbb7498cfe3a3d11e6650c0be3e85e223
EdiiA <96665518+EdisonAlushaj@users.noreply.github.com>
2024-03-25 17:25:29 +0100

    header

--- Hoteli/src/Header.jsx
+++ Hoteli/src/Header.jsx
@@ -2,13 +2,13 @@
 function Header() {
   return (
     <header className="App-header">
-    <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Bookings</li>
-        <li>Services</li>
-        <li>Contact Us</li>
-    </ul>
+      <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Bookings</li>
+          <li>Services</li>
+          <li>Contact Us</li>
+      </ul>
     </header>
   );
 }
