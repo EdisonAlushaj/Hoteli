b3d209d533911c0bf866a7265d92377b4978e068
oltahasimja <oh67751@ubt-uni.net>
2024-03-25 17:17:08 +0100

    Header
    Krijimi i header

---
+++ Hoteli/src/Header.jsx
@@ -0,0 +1,16 @@
+
+function Header() {
+  return (
+    <header className="App-header">
+    <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Bookings</li>
+        <li>Services</li>
+        <li>Contact Us</li>
+    </ul>
+    </header>
+  );
+}
+
+export default Header;
