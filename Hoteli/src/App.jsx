b320d1791f1a0115670e3db294e3b977aa31992e
invadoli <id67745@ubt-uni.net>
2024-03-25 20:50:32 +0100

    Footer

--- Hoteli/src/App.jsx
+++ Hoteli/src/App.jsx
@@ -1,4 +1,5 @@
 import Header from './Header'
+import Footer from './Footer'
 import './App.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Header/>
+      <Footer/>
     </>
   )
 }
