662930bbc4eb8745e73f3d46685cd0ef3e61bbe8
invadoli <id67745@ubt-uni.net>
2024-03-29 00:26:37 +0100

    Importimi i Coverit
    Shtimi i nje teksi ne home

--- Hoteli/src/Home.jsx
+++ Hoteli/src/Home.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Slider from './Slider/slider.jsx';
+import Cover from './Home/cover.jsx';
 
 function Home (){
   return (
     <>
       <Slider />
+      <Cover />
     </>
   );
 };
