2cad8086f48eab89e55a6aec519091eea4af36c2
elez-kadrijaj <150354782+elezkadrijaj@users.noreply.github.com>
2024-04-01 01:41:37 +0200

    Coomit

--- Hoteli/src/Home.jsx
+++ Hoteli/src/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Slider from './Slider/slider.jsx';
 import Cover from './Home/cover.jsx';
-// import Slider3D from './3DSlider.jsx'
+import Slider3D from './3DSlider.jsx'
 import SlideR from './3DSlide.jsx'
 
 function Home (){
@@ -10,7 +10,7 @@ function Home (){
       <Slider />
       <Cover />
       
-      <SlideR/>
+      <Slider3D />
     </>
   );
 };
