ce5ccf63d273a0cdfdd20616cd166d060ef5f36a
elez-kadrijaj <150354782+elezkadrijaj@users.noreply.github.com>
2024-04-01 01:40:56 +0200

    nje model tjeter i 3d Slide show

--- Hoteli/src/Home.jsx
+++ Hoteli/src/Home.jsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import Slider from './Slider/slider.jsx';
 import Cover from './Home/cover.jsx';
+// import Slider3D from './3DSlider.jsx'
+import SlideR from './3DSlide.jsx'
 
 function Home (){
   return (
     <>
       <Slider />
       <Cover />
+      
+      <SlideR/>
     </>
   );
 };
