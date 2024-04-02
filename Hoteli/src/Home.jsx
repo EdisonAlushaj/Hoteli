07a33c422eb6a4afabf7a9592eeebcda5db9fb82
elez-kadrijaj <150354782+elezkadrijaj@users.noreply.github.com>
2024-04-02 23:08:16 +0200

    Home ndryshime
    disa shtesa ne home

--- Hoteli/src/Home.jsx
+++ Hoteli/src/Home.jsx
@@ -1,16 +1,44 @@
 import React, { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import Slider from './Slider/slider.jsx';
 import Cover from './Home/cover.jsx';
-import Slider3D from './3DSlider.jsx'
+
 import SlideR from './3DSlide.jsx'
 
-function Home (){
+function Home() {
   return (
     <>
       <Slider />
       <Cover />
-      
+
       <SlideR />
+      <Container style={{marginLeft:'2em'}}>
+        <h1 style={{ fontFamily: 'Roboto Slab, serif', fontSize: '6em' }} className='mb-5'>Amenities</h1>
+        <Row className="align-items-start">
+          <Col>
+            <h2 style={{fontFamily: 'Roboto Slab, serif'}} className='mb-4'>Reception 24/7</h2>
+            <p style={{color: 'gray'}}>Our friendly staff is at your service</p>
+            <p style={{color: 'gray'}}>24 hours a day at the reception.</p>
+          </Col>
+          <Col>
+            <h2 style={{fontFamily: 'Roboto Slab, serif'}} className='mb-4'>Parking</h2>
+            <p style={{color: 'gray'}}>Underground parking available for</p>
+            <p style={{color: 'gray'}}>our clients, for free, 24 hours a day.</p>
+          </Col>
+          <Col>
+            <h2 style={{fontFamily: 'Roboto Slab, serif'}} className='mb-4'>Restaurant</h2>
+            <p style={{color: 'gray'}}>The restaurant, where guests can</p>
+            <p style={{color: 'gray'}}>have their breakfast, is located on</p>
+            <p style={{color: 'gray'}}>the ground floor.</p>
+          </Col>
+          <Col>
+            <h2 style={{fontFamily: 'Roboto Slab, serif'}} className='mb-4'>Lobby</h2>
+            <p style={{color: 'gray'}}>Derand Hotel features a modern</p>
+            <p style={{color: 'gray'}}>lobby to warmly welcome its guests</p>
+            <p style={{color: 'gray'}}>with comfortable seating.</p>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
