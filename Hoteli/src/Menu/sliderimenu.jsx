dfe2a7902c7951ed2c0508c69de7a701b4f961a8
oltahasimja <oh67751@ubt-uni.net>
2024-04-03 02:17:05 +0200

    Permisime ne slider

--- Hoteli/src/Menu/sliderimenu.jsx
+++ Hoteli/src/Menu/sliderimenu.jsx
@@ -2,21 +2,60 @@ import React, { useState, useEffect } from 'react';
 import './slideshow.css';
 import foto1 from './foto1.jpg';
 import foto2 from './foto2.jpg';
-import foto3 from './foto3.jpg';
+import foto3 from './foto3.jpeg';
 
-const TextOverlay = ({ text, top, left }) => {
+const TextOverlay = ({ text, top, left, style }) => {
   return (
-    <div className="text-overlay" style={{ top, left }}>
+    <div className="text-overlay" style={{ top, left, ...style , height:'3.5em'}}>
       <p>{text}</p>
     </div>
   );
 };
 
+
 const Slider = () => {
   const imagesWithText = [
-    { src: foto1, text: "Traditional Spanish dish with a variety of fresh seafood and rice." },
-    { src: foto2, text: "Enjoy refreshing drinks at Ibiza Drinks Bar." },
-    { src: foto3, text: "Indulge in a selection of coffee and sweet treats." },
+    {
+      src: foto1,
+      text: "Traditional Spanish dish with a variety of fresh seafood and rice.",
+      style: {
+      
+        color: '#F5F5F5',
+        borderRadius: '5px',
+        padding: '10px',
+        left: '60%',
+        top: '30%',
+   
+      },
+    },
+    {
+      src: foto2,
+      text: "Enjoy refreshing drinks at Ibiza Drinks Bar.",
+      style: {
+        backgroundColor: '#ffcc99',
+        color: '#333',
+        borderRadius: '5px',
+        padding: '10px',
+        left: '60%',
+        top: '30%',
+        width: '12em',
+        height: '15em',
+      },
+    },
+    {
+      src: foto3,
+      text: "Indulge in a selection of coffee and sweet treats.",
+      style: {
+        backgroundColor: '#ffcc99',
+        color: '#333',
+        borderRadius: '5px',
+        padding: '10px',
+        left: '45%',
+        top: '30%',
+        width: '25em',
+        height: '15em',
+      },
+    },
   ];
 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -45,10 +84,10 @@ const Slider = () => {
       <button className="prev" onClick={handlePrevButtonClick}>
         {currentImageIndex === 0 ? '' : ''}
       </button>
-      {imagesWithText.map(({ src, text }, index) => (
+      {imagesWithText.map(({ src, text, style }, index) => (
         <div key={index}>
           <img src={src} alt={`Slide ${index + 1}`} className={index === currentImageIndex ? 'active' : ''} />
-          {currentImageIndex === index && <TextOverlay text={text} top="80%" left="10%" />}
+          {currentImageIndex === index && <TextOverlay text={text} top="80%" left="10%" style={style} />}
         </div>
       ))}
       <button className="next" onClick={handleNextButtonClick}>
