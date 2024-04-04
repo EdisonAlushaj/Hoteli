35f2c629895e9d0f6f81d9083a87e3fcf6f74d14
ArianitKadrijaj <151252306+ArianitKadrijaj@users.noreply.github.com>
2024-04-05 01:55:27 +0200

    About Us
    Shtimi i nje texti dhe fotos

--- Hoteli/src/About.jsx
+++ Hoteli/src/About.jsx
@@ -1,19 +1,33 @@
 import React, { Component } from "react";
 import Foto from './assets/about.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Foto1 from './image/foto-1.jpg';
+import { Container, Row, Col } from 'react-bootstrap';
+import './About-us/about-us.css'
 
 class About extends Component {
   render() {
     return (
       <>
         <div className="h">
-          <img src={Foto} className="w-100" style={{ height: '50em' }} alt="Cover Image" />
+          <img src={Foto} className="w-100" style={{ height: '30em', objectFit: "cover" }} alt="Cover Image" />
         </div>
         <h1 className="h1 fade-in-left" style={{ color: '#b07256' }}>About Us</h1>
-        <textarea name="" id="" cols="100" rows="10" style={{ border: 0 }}>Perched majestically in the center of the city, our hotel stands as a testament to Prishtina’s rich tapestry of history and its dynamic future. A seamless blend of modern elegance and cherished traditions, our seven-story haven invites guests to experience the best of both worlds.
-          As you step into our world, you’ll find yourself enveloped in an atmosphere that seamlessly fuses the allure of contemporary sophistication with the echoes of a cultural heritage that stretches back in time. Our commitment to you is nothing short of delivering an experience that exceeds your expectations – a promise that finds its roots in Prishtina’s very soul.
-          At the heart of our ethos is an unwavering commitment to provide you with an essence of
-          luxury. This pledge is more than just a statement; it is a lived experience. Every interaction, every service, and every gesture by our dedicated team is meticulously crafted to immerse you in a</textarea>
+        <div className="d-flex justify-content-around align-items-center">
+          <Row>
+            <Col xs={5} md={6} className="" style={{ width: '25em', marginLeft: '10em' }}>
+              <div style={{marginLeft:'2em'}}>
+                <h1>Our Mission</h1>
+                <p>Our hotel, a distinguished accommodation center, aims to provide an excellent experience for every visitor. Located in a splendid setting in the heart of [location], our mission is to offer a warm and welcoming environment, ensuring that every guest feels greeted and cared for. With a continuous commitment to excellent service and attention to detail, we strive to make each visitor's stay memorable. Our dedicated team is here to ensure that every request is addressed with care and to create an exceptional experience for all who choose to stay with us.</p>
+              </div>
+            </Col>
+            <Col xs={12} md={4}>
+              <img src={Foto1} alt="Image" className="w-100" style={{ height: '30em', marginLeft: '25em' }} />
+            </Col>
+          </Row>
+        </div>
+      
+
       </>
     );
   }
