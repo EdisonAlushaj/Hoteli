785fc5bed61c20fbc13752b32fd139e4d9a680ed
oltahasimja <oh67751@ubt-uni.net>
2024-04-05 13:39:23 +0200

    Rregullimi i pozicionit te butonit te rezertvimit te tavolinave si dhe stilizimi i butonit

--- Hoteli/src/Menu/Menu.jsx
+++ Hoteli/src/Menu/Menu.jsx
@@ -8,6 +8,7 @@ import MenuDrinks from '/src/Menu/menudrinks.jsx';
 import MenuFood from '/src/Menu/menufood.jsx';
 import Reservation from './Reservation.jsx';
 import rezervimimg from './reserviminew.jpg';
+
 function Menu() {
   const [showReservation, setShowReservation] = useState(false);
   const [activeMenu, setActiveMenu] = useState(<MenuFood />);
@@ -35,20 +36,26 @@ function Menu() {
   return (
     <>
       <Slider onMenuChange={handleMenuChange} />
-      <Container fluid style={{ backgroundColor: '#f6f5dc' }}>
+      <Container fluid style={{ backgroundColor: '#f6f5dc', position: 'relative' }}>
         <Row>{activeMenu}</Row>
-        <img src={rezervimimg} alt=""  style={{width: '100%', height: '', objectFit: ''}}/>
-        <Row className="mt-5">
-          <Col>
-            {/* Reservation Button */}
-              
-            <Button variant="info" onClick={toggleReservation}>
-
-              Make Reservation
-            </Button>
-            {showReservation && <Reservation />}
-          </Col>
-        </Row>
+        <img src={rezervimimg} alt="" style={{ width: '100%', height: '50em', objectFit: '' }} />
+        <Button onClick={toggleReservation} style={{ 
+  position: 'absolute', 
+  bottom: '11.3em', 
+  left: '50%', 
+  transform: 'translateX(-50%)', 
+  zIndex: 1,
+  backgroundColor: 'transparent', 
+  color: '#b07256',  
+  fontFamily:  'Roboto Slab, serif',
+  borderColor: 'transparent',
+  fontSize: '35px',
+  transition: 'color 0.3s', 
+  cursor: 'pointer' 
+}}>
+  Reserve a Table
+</Button>
+        {showReservation && <Reservation />}
       </Container>
     </>
   );
