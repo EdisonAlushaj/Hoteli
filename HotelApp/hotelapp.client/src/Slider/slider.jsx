55c98e77072baf7d25e1f0792d9205f5072f0a21
EdiiA <96665518+EdisonAlushaj@users.noreply.github.com>
2024-04-08 00:14:06 +0200

    Rregullimi i formatiti te projektit.
    Shtimi i lidhjes me .Net + React.Js.
    
    Co-Authored-By: ArianitKadrijaj <151252306+ArianitKadrijaj@users.noreply.github.com>
    Co-Authored-By: elezkadrijaj <150354782+elezkadrijaj@users.noreply.github.com>
    Co-Authored-By: invadoli <149635410+invadoli@users.noreply.github.com>
    Co-Authored-By: oltahasimja <149635414+oltahasimja@users.noreply.github.com>

---
+++ HotelApp/hotelapp.client/src/Slider/slider.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import './slideshow.css';
+import foto1 from './foto-1.jpg';
+import foto2 from './foto-2.jpg';
+import foto3 from './foto-3.jpg';
+import foto4 from './foto-4.jpg';
+
+const Slider = () => {
+  const images = [foto1, foto2, foto3, foto4];
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 6000); 
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleButtonClick = (newIndex) => {
+    if (newIndex >= 0 && newIndex < images.length) {
+      setCurrentImageIndex(newIndex);
+    }
+  };
+
+  const handlePrevButtonClick = () => {
+    const newIndex = currentImageIndex === 0 ? images.length - 1 : currentImageIndex - 1;
+    setCurrentImageIndex(newIndex);
+  };
+
+  const handleNextButtonClick = () => {
+    const newIndex = currentImageIndex === images.length - 1 ? 0 : currentImageIndex + 1;
+    setCurrentImageIndex(newIndex);
+  };
+
+  return (
+    <div className="slider">
+      {images.map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          alt={`Slide ${index + 1}`}
+          className={index === currentImageIndex ? 'active' : ''}
+        />
+      ))}
+      <div className="slider-buttons">
+        {images.map((_, index) => (
+          <button
+            key={index}
+            className="{index === currentImageIndex ? 'active' : ''} btn"
+            style={{}}
+            onClick={() => handleButtonClick(index)}
+          >
+            <b>{index + 1}</b>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
