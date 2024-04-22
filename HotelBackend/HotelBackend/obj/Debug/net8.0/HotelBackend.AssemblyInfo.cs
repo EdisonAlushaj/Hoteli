c0676a1d55518cc9ad61de82ce4ff401b8045e2d
EdiiA <96665518+EdisonAlushaj@users.noreply.github.com>
2024-04-22 15:40:27 +0200

    Comit test database
    Comit per testim te database

--- HotelBackend/HotelBackend/obj/Debug/net8.0/HotelBackend.AssemblyInfo.cs
+++ HotelBackend/HotelBackend/obj/Debug/net8.0/HotelBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f7ccc038746a98211b2c772cd54edf205a37399")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+982fcca2b53fd98d73aef6877553903811ee37c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
